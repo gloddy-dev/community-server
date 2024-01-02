9134ac405733d6e1afcdbd5baae503dd437de0c4
twoosky <lhjae3@naver.com>
2024-01-02 22:59:13 +0900

    feat: comment 엔티티 작성

---
+++ community-infrastructure/src/main/kotlin/gloddy/rdb/comment/CommentEntity.kt
@@ -0,0 +1,28 @@
+package gloddy.rdb.comment
+
+import gloddy.rdb.common.entity.BaseTimeEntity
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "comment")
+class CommentEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private val id: Long = 0L,
+
+    @Column(name = "user_id", nullable = false)
+    private val userId: Long,
+
+    @Column(name = "article_id", nullable = false)
+    private val articleId: Long,
+
+    @Column(name = "like_count", nullable = false)
+    private val likeCount: Long,
+
+    @Column(name = "depth", nullable = false)
+    private val depth: Int,
+
+    @Column(name = "ref", nullable = false)
+    private val ref: Int
+
+): BaseTimeEntity()

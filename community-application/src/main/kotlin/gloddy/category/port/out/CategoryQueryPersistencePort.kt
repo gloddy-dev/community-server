f030a941aae09be7fbceb7ab8f7334a7f18bf1a8
jihwan2da <wlghks8982@naver.com>
2024-01-05 15:50:31 +0900

    feat: CategoryQueryPersistenceAdapter 생성 및 findById 메소드 작성

---
+++ community-application/src/main/kotlin/gloddy/category/port/out/CategoryQueryPersistencePort.kt
@@ -0,0 +1,8 @@
+package gloddy.category.port.out
+
+import gloddy.category.Category
+import gloddy.core.CategoryId
+
+interface CategoryQueryPersistencePort {
+    fun findById(id: CategoryId): Category
+}
---
+++ community-infrastructure/src/main/kotlin/gloddy/persistence/category/adapter/CategoryQueryPersistenceAdapter.kt
@@ -0,0 +1,20 @@
+package gloddy.persistence.category.adapter
+
+import gloddy.category.Category
+import gloddy.category.exception.CategoryNotFoundException
+import gloddy.category.port.out.CategoryQueryPersistencePort
+import gloddy.core.CategoryId
+import gloddy.persistence.category.repository.CategoryJpaRepository
+import gloddy.persistence.util.mapper.toDomain
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
+
+@Component
+class CategoryQueryPersistenceAdapter(
+    private val categoryJpaRepository: CategoryJpaRepository,
+) : CategoryQueryPersistencePort {
+
+    override fun findById(id: CategoryId): Category {
+        return (categoryJpaRepository.findByIdOrNull(id.value) ?: throw CategoryNotFoundException()).toDomain()
+    }
+}

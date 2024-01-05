9f3375cbdb533febb6787b476298ff2704c1efee
jihwan2da <wlghks8982@naver.com>
2024-01-05 19:07:55 +0900

    feat: Article 도메인 권한 관련 예외 및 예외코드 작성

--- community-domain/src/main/kotlin/gloddy/article/ArticleErrorCode.kt
+++ community-domain/src/main/kotlin/gloddy/article/ArticleErrorCode.kt
@@ -8,5 +8,6 @@ enum class ArticleErrorCode(
     override val message: String
 ) : ErrorCode {
     NOT_FOUND(400, "ARTICLE_001", "해당 게시글을 찾을 수 없습니다."),
-    IMAGE_SIZE_OVER(400, "ARTICLE_002", "게시글에 이미지는 최대 3개 입니다.")
+    IMAGE_SIZE_OVER(400, "ARTICLE_002", "게시글에 이미지는 최대 3개 입니다."),
+    NO_AUTHORIZATION(401, "ARTICLE_003", "해당 게시글에 권한이 없습니다.")
 }

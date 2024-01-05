c213c7a5fcbc669f7b3c6b9b2d479cbe32d6990a
jihwan2da <wlghks8982@naver.com>
2024-01-05 15:51:44 +0900

    test: Application 모듈 테스트 환경 구축

---
+++ community-application/src/test/kotlin/gloddy/ServiceTest.kt
@@ -0,0 +1,8 @@
+package gloddy
+
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.junit.jupiter.MockitoExtension
+
+@ExtendWith(MockitoExtension::class)
+abstract class ServiceTest {
+}

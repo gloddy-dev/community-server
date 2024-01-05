454163b11b4dcc7d9e3227fef0b3666fb6ce3564
jihwan2da <wlghks8982@naver.com>
2024-01-05 20:59:58 +0900

    fix: ArticeLike 도메인 생성 검증 추가(Article의 영속성)

--- community-domain/src/main/kotlin/gloddy/article/ArticleLike.kt
+++ community-domain/src/main/kotlin/gloddy/article/ArticleLike.kt
@@ -6,4 +6,14 @@ data class ArticleLike(
     val userId: UserId,
     val article: Article,
     val id: Long? = null,
-)
+) {
+    init {
+        verifyArticle(article)
+    }
+
+    private fun verifyArticle(article: Article) {
+        if (article.id == null) {
+            throw RuntimeException("ArticleLike 생성 오류 : 영속화 되지 않은 Article을 입력으로 받았습니다.")
+        }
+    }
+}

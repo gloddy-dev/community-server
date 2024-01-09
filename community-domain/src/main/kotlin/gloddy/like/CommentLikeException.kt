e8c469db849213fb5cbdcb6c0f70a7612fda81c0
twoosky <lhjae3@naver.com>
2024-01-09 23:55:29 +0900

    feat: comment 좋아요 도메인 작성

---
+++ community-domain/src/main/kotlin/gloddy/like/CommentLikeException.kt
@@ -0,0 +1,10 @@
+package gloddy.like
+
+import gloddy.core.ErrorCode
+import gloddy.core.GloddyCommunityException
+
+class CommentLikeNotFoundException : GloddyCommunityException(
+    statusCode = 404,
+    errorCode = ErrorCode.COMMENT_LIKE_NOT_FOUND,
+    message = "존재하지 않는 댓글 좋아요입니다."
+)

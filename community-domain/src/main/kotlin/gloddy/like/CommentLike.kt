989f2cd38f1793f5fd384aa4cf440bee56586a92
twoosky <lhjae3@naver.com>
2024-01-09 23:55:29 +0900

    feat: comment 좋아요 도메인 작성

--- community-domain/src/main/kotlin/gloddy/like/CommentLike.kt
+++ community-domain/src/main/kotlin/gloddy/like/CommentLike.kt
@@ -5,17 +5,8 @@ import gloddy.comment.Comment
 @JvmInline
 value class CommentLikeId(val value: Long)
 
-@JvmInline
-value class ArticleLikeId(val value: Long)
-
 data class CommentLike(
     val id: CommentLikeId? = CommentLikeId(0L),
     val user: User,
     val comment: Comment
-)
-
-data class ArticleLike(
-    val id: ArticleLikeId? = ArticleLikeId(0L),
-    val user: User,
-    val article: Article
 )

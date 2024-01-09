95ce34876975370f391ccae4183a1d01d378ca60
twoosky <lhjae3@naver.com>
2024-01-09 23:56:42 +0900

    chore: 기본 생성자 생성 플러그인 추가

--- community-infrastructure/build.gradle.kts
+++ community-infrastructure/build.gradle.kts
@@ -15,14 +15,14 @@ jar.enabled = true
 extra["springCloudVersion"] = "2023.0.0"
 
 plugins {
-    kotlin("plugin.jpa") version "1.9.20"
+    kotlin("plugin.jpa") version "1.9.22"
 }
 
-allOpen {
-    annotation("jakarta.persistence.Entity")
-    annotation("jakarta.persistence.Embeddable")
-    annotation("jakarta.persistence.MappedSuperclass")
-}
+//allOpen {
+//    annotation("jakarta.persistence.Entity")
+//    annotation("jakarta.persistence.Embeddable")
+//    annotation("jakarta.persistence.MappedSuperclass")
+//}
 
 dependencies {
     implementation(project(":community-application"))

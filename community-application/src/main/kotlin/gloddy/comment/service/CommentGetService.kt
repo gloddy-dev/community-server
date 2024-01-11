84bde6dc35ca66dcbb0800941300eddd6cd56315
twoosky <lhjae3@naver.com>
2024-01-11 23:01:40 +0900

    feat: article에 대한 댓글 조회 비즈니스 로직 구현

--- community-application/src/main/kotlin/gloddy/comment/service/CommentGetService.kt
+++ community-application/src/main/kotlin/gloddy/comment/service/CommentGetService.kt
@@ -1,19 +1,19 @@
 package gloddy.comment.service
 
+import gloddy.article.port.out.ArticleQueryPersistencePort
 import gloddy.comment.Comment
 import gloddy.comment.dto.CommentGetRequest
+import gloddy.comment.dto.readModel.CommentFindByArticleDto
 import gloddy.comment.port.out.CommentQueryPort
 import org.springframework.stereotype.Service
 
 @Service
 class CommentGetService(
+    private val articleQueryPersistencePort: ArticleQueryPersistencePort,
     private val commentQueryPort: CommentQueryPort
 ) {
-    fun getByArticle(dto: CommentGetRequest) {
-        val comments = commentQueryPort.findAllByArticleId(dto.articleId)
-
-        val commentGroupByRef: Map<Int, List<Comment>> = comments.groupBy { it.ref }
-            .map {  }
-        commentGroupByRef.
+    fun findByArticle(dto: CommentGetRequest): List<CommentFindByArticleDto> {
+        val article = articleQueryPersistencePort.findById(dto.articleId.value)
+        return commentQueryPort.findAllByArticle(article, dto.userId.value)
     }
 }

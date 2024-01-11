19312d9d315d75b94495685dcc1469ec8ea22e53
twoosky <lhjae3@naver.com>
2024-01-11 23:01:04 +0900

    feat: 부모, 자식 댓글 생성 비즈니스로직 구현

--- community-application/src/main/kotlin/gloddy/comment/service/CommentCreateService.kt
+++ community-application/src/main/kotlin/gloddy/comment/service/CommentCreateService.kt
@@ -1,47 +1,57 @@
 package gloddy.comment.service
 
+import gloddy.article.port.out.ArticleQueryPersistencePort
 import gloddy.comment.Comment
 import gloddy.comment.dto.ChildCommentCreateRequest
+import gloddy.comment.dto.CommentCreateResponse
 import gloddy.comment.dto.ParentCommentCreateRequest
 import gloddy.comment.port.out.CommentCommandPort
 import gloddy.comment.port.out.CommentQueryPort
+import gloddy.user.port.out.UserQueryPort
 import org.springframework.stereotype.Service
 
 @Service
 class CommentCreateService(
     private val commentCommandPort: CommentCommandPort,
     private val commentQueryPort: CommentQueryPort,
-    private val articleGetPort: ArticleGetPort,
-    private val userGetPort: UserGetPort
+    private val articleGetPort: ArticleQueryPersistencePort,
 ) {
-    fun createParent(dto: ParentCommentCreateRequest) {
-        val article = articleGetPort.findById(dto.articleId)
-        val user = userGetPort.findById(dto.userId)
+    fun createParent(dto: ParentCommentCreateRequest): CommentCreateResponse {
+        val article = articleGetPort.findById(dto.articleId.value)
+        val ref = commentQueryPort.findMaxRefByArticle(article).maxRef + 1
 
-        val ref = commentQueryPort.getMaxRef().maxRef + 1
-
-        Comment(
-            user = user,
+        return Comment(
+            userId = dto.userId,
             article = article,
             content = dto.content,
             depth = 0,
             ref = ref
-        ).run { commentCommandPort.save(this) }
+        )
+        .let {
+            commentCommandPort.save(it)
+        }
+        .run {
+            CommentCreateResponse(this)
+        }
     }
 
-    fun createChild(dto: ChildCommentCreateRequest) {
-        val article = articleGetPort.findById(dto.articleId)
-        val user = userGetPort.findById(dto.userId)
-
-        val parentInfo = commentQueryPort.getParentRef()
+    fun createChild(dto: ChildCommentCreateRequest): CommentCreateResponse {
+        val article = articleGetPort.findById(dto.articleId.value)
+        val parentInfo = commentQueryPort.findById(dto.parentCommentId)
         val depth = parentInfo.depth + 1
 
-        Comment(
-            user = user,
+        return Comment(
+            userId = dto.userId,
             article = article,
             content = dto.content,
             depth = depth,
             ref = parentInfo.ref
-        ).run { commentCommandPort.save(this) }
+        )
+        .let {
+            commentCommandPort.save(it)
+        }
+        .run {
+            CommentCreateResponse(this)
+        }
     }
 }

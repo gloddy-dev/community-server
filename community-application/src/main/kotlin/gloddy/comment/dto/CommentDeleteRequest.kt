93176796b1ce61359434e6ac8db3c61759352ecf
twoosky <lhjae3@naver.com>
2024-01-11 23:03:01 +0900

    feat: comment request, response 정의 및 readModel dto 정의

--- community-application/src/main/kotlin/gloddy/comment/dto/CommentDeleteRequest.kt
+++ community-application/src/main/kotlin/gloddy/comment/dto/CommentDeleteRequest.kt
@@ -1,9 +1,11 @@
 package gloddy.comment.dto
 
+import gloddy.comment.ArticleId
 import gloddy.comment.CommentId
 import gloddy.user.UserId
 
 data class CommentDeleteRequest(
     val commentId: CommentId,
-    val userId: UserId
+    val userId: UserId,
+    val articleId: ArticleId
 )

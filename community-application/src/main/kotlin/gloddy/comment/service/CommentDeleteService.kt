7c66298f4de5333a1b23d12d93c14cf8091ae849
twoosky <lhjae3@naver.com>
2024-01-11 23:01:20 +0900

    feat: 댓글 삭제 비즈니스 로직 구현

--- community-application/src/main/kotlin/gloddy/comment/service/CommentDeleteService.kt
+++ community-application/src/main/kotlin/gloddy/comment/service/CommentDeleteService.kt
@@ -5,25 +5,26 @@ import gloddy.comment.CommentNotAuthorizationException
 import gloddy.comment.dto.CommentDeleteRequest
 import gloddy.comment.port.out.CommentCommandPort
 import gloddy.comment.port.out.CommentQueryPort
+import gloddy.user.User
+import gloddy.user.UserId
+import gloddy.user.port.out.UserQueryPort
 import org.springframework.stereotype.Service
 
 @Service
 class CommentDeleteService(
     private val commentCommandPort: CommentCommandPort,
-    private val commentQueryPort: CommentQueryPort
-    private val userQueryPort: UserQueryPort
+    private val commentQueryPort: CommentQueryPort,
 ) {
 
     fun delete(dto: CommentDeleteRequest) {
-        val user = userQueryPort.findById(dto.userId)
         val comment = commentQueryPort.findById(dto.commentId)
 
-        isWriter(comment, user)
+        isWriter(comment, dto.userId)
         commentCommandPort.delete(comment)
     }
 
-    fun isWriter(comment: Comment, user: User) {
-        if (comment.isWriter(user).not()) {
+    fun isWriter(comment: Comment, userId: UserId) {
+        if (comment.isWriter(userId).not()) {
             throw CommentNotAuthorizationException()
         }
     }
